4b29be5ed7b072e97b8d6934a1cbde03b98e0242
BilalYassine <byassine@wisc.edu>
2020-04-06 19:10:11 -0400

    Added Main Activity Button Hub + Added Join Party Activity UI

---
+++ app/src/main/java/com/example/youqueue/JoinPartyActivity.java
@@ -0,0 +1,16 @@
+package com.example.youqueue;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinPartyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_party);
+    }
+
+}
--- app/src/main/java/com/example/youqueue/MainActivity.java
+++ app/src/main/java/com/example/youqueue/MainActivity.java
@@ -2,9 +2,26 @@ package com.example.youqueue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void goToSettings(View view) {
+        Log.i("Info", "Settings Button pressed");
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToJoinParty(View view) {
+        Log.i("Info", "Settings Button pressed");
+        Intent intent = new Intent(this, JoinPartyActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
