4d7bfa793cb540716c336a164bdad5c7c7d5012c
Simon <smtheuer@gmail.com>
2020-04-13 12:13:14 -0700

    Can play song given uri, can pause and resume playback, can queue song.

--- app/src/main/java/com/example/youqueue/MainActivity.java
+++ app/src/main/java/com/example/youqueue/MainActivity.java
@@ -8,8 +8,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.ListItems;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+import com.spotify.protocol.types.Repeat;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "d19dfd48fcd54626a0f8ff696ada3b9e";
+    private static final String REDIRECT_URI = "com.youqueue://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
 
     public void goToSettings(View view) {
         Log.i("Info", "Settings Button pressed");
@@ -34,4 +50,81 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Set the connection parameters
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+                        connected();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    private void connected() {
+        //queueSong("spotify:track:5nrmGFJ87crVoJF5xdRqwn");
+        //resumePlayback();
+        //pausePlayback();
+        //playSong("spotify:track:1Eolhana7nKHYpcYpdVcT5");
+        //search("Jude");
+        /*
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                });
+
+         */
+    }
+
+    private void playSong(String uri){
+        mSpotifyAppRemote.getPlayerApi().play(uri);
+    }
+
+    private void queueSong(String uri){
+        mSpotifyAppRemote.getPlayerApi().queue(uri);
+    }
+
+    private void pausePlayback(){
+        mSpotifyAppRemote.getPlayerApi().pause();
+    }
+
+    private void resumePlayback(){ mSpotifyAppRemote.getPlayerApi().resume(); }
+
+    private void search(String track){
+
+    }
+
 }
