954bcdd598679d1574cd499c2b0deb00203befd1
BilalYassine <byassine@wisc.edu>
2020-04-22 23:21:19 -0400

    Temporary XML Implementations for Settings, StartParty, JoinParty

--- app/src/main/java/com/example/youqueue/SettingsActivity.java
+++ app/src/main/java/com/example/youqueue/SettingsActivity.java
@@ -2,7 +2,10 @@ package com.example.youqueue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void goHome(View view) {
+        Log.i("Info", "Back Button pressed");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
